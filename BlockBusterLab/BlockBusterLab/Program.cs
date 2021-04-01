27f2b08fda1ea46ba45940dd0aa6050d0a5b88ff
gesawyer <74635177+gesawyer@users.noreply.github.com>
2021-03-31 21:17:45 -0400

    Fixed a silly bug and some formatting

--- BlockBusterLab/BlockBusterLab/Program.cs
+++ BlockBusterLab/BlockBusterLab/Program.cs
@@ -8,20 +8,22 @@ namespace BlockBusterLab
     {
         static void Main(string[] args)
         {
+            List<Movie> Movies = new List<Movie>();
             Blockbuster b = new Blockbuster();
             b.PrintMovies();
-            Console.WriteLine("Welcome to the last Blockbuster on earth. Here are all the movies we " +
-               "have available. What movie would you like to rent today?");
-            string input = Console.ReadLine();
-            int a = int.Parse(input);
-            b.CheckOut(a);
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the last Blockbuster on earth. Here are all the movies we have available. " +
+                "What movie would you like to rent today?");
+            //string input = Console.ReadLine();
+            //int a = int.Parse(input);
+            b.CheckOut();
             //b.PrintScenes();
         }
-            
-            
-           
-            
-        
+
+
+
+      
+
 
     }
 
